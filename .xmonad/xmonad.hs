eedf3d8fbd0a1150fb6141a0aeb82353da3daf11
thetiiiim <0timothy.scott0@gmail.com>
2022-08-12 18:48:10 -0400

    add gaps to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,6 @@
 import XMonad
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
 import XMonad.Hooks.DynamicLog
 
 main = xmonad =<< xmobar def
@@ -6,5 +8,8 @@ main = xmonad =<< xmobar def
     , modMask     = mod4Mask
     , borderWidth = 3
     , startupHook = spawn "~/.xmonad/autostart"
+    , normalBorderColor = "#431751"
+    , focusedBorderColor = "#49A7C6"
+    , layoutHook = smartSpacingWithEdge 5 $ Tall 1 (3/100) (1/2)
     }
 
